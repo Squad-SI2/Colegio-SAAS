9664becc2de10beda63d0730ec0c000f4240f841
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-09-16 14:29:00 -0400

    fix hot reloading in frontend with vitejs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,6 +12,9 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    watch: {
+      usePolling: true,
+    },
     proxy: {
       // Reenvía /api al backend por el nombre del servicio dentro de la red Docker
       '/api': {
